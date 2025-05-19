bf8eeb5c5ec61ab052ba8226410fc968b095005d
Marco Weiss <marcoweiss22@gmail.com>
2025-05-19 00:06:00 -0300

    Add files via upload

---
+++ Pila-TP13.py
@@ -0,0 +1,63 @@
+def modelo_hulkbuster(pila):
+    aux = Pila()
+    encontrado = False
+    print("Películas con el modelo Mark XLIV (Hulkbuster):")
+    while not pila.esta_vacia():
+        traje = pila.desapilar()
+        if traje["modelo"] == "Mark XLIV":
+            print(f"- {traje['pelicula']}")
+            encontrado = True
+        aux.apilar(traje)
+    while not aux.esta_vacia():
+        pila.apilar(aux.desapilar())
+    if not encontrado:
+        print("No se encontró el modelo Hulkbuster.")
+
+def mostrar_danados(pila):
+    aux = Pila()
+    print("Modelos que quedaron dañados:")
+    while not pila.esta_vacia():
+        traje = pila.desapilar()
+        if traje["estado"] == "Dañado":
+            print(f"- {traje['modelo']} ({traje['pelicula']})")
+        aux.apilar(traje)
+    while not aux.esta_vacia():
+        pila.apilar(aux.desapilar())
+
+def eliminar_destruidos(pila):
+    aux = Pila()
+    print("Modelos destruidos eliminados:")
+    while not pila.esta_vacia():
+        traje = pila.desapilar()
+        if traje["estado"] == "Destruido":
+            print(f"- {traje['modelo']} ({traje['pelicula']})")
+        else:
+            aux.apilar(traje)
+    while not aux.esta_vacia():
+        pila.apilar(aux.desapilar())
+
+def agregar_modelo(pila, modelo, pelicula, estado):
+    aux = Pila()
+    repetido = False
+    while not pila.esta_vacia():
+        traje = pila.desapilar()
+        if traje["modelo"] == modelo and traje["pelicula"] == pelicula:
+            repetido = True
+        aux.apilar(traje)
+    if not repetido:
+        aux.apilar({"modelo": modelo, "pelicula": pelicula, "estado": estado})
+    else:
+        print(f"Ya existe el modelo {modelo} en la película {pelicula}")
+    while not aux.esta_vacia():
+        pila.apilar(aux.desapilar())
+
+def mostrar_trajes_peliculas(pila, peliculas):
+    aux = Pila()
+    print("Trajes usados en películas seleccionadas:")
+    while not pila.esta_vacia():
+        traje = pila.desapilar()
+        if traje["pelicula"] in peliculas:
+            print(f"- {traje['modelo']} ({traje['pelicula']})")
+        aux.apilar(traje)
+    while not aux.esta_vacia():
+        pila.apilar(aux.desapilar())
