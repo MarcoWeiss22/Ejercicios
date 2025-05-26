0e5c7237c4409e0d3e98ca93dff7c67a37ed4c32
Marco Weiss <marcoweiss22@gmail.com>
2025-05-26 20:41:19 -0300

    Update Recursividad-TP5.py

--- Recursividad-TP5.py
+++ Recursividad-TP5.py
@@ -1,22 +1,33 @@
-def romano_a_decimal(romano):
-    valores = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
-    }
-    
-    total = 0
-    anterior = 0
-    
-    for letra in reversed(romano):
-        valor = valores[letra]
-        if valor < anterior:
-            total -= valor
-        else:
-            total += valor
-        anterior = valor
-    
-    return total
 
-# Ejemplo para tener una idea de como funciona:
-numero_romano = "MCMXCIV"  # 1994
-print(romano_a_decimal(numero_romano))
+from stack import Stack
+
+roman_values = {
+    'I': 1, 'V': 5, 'X': 10,
+    'L': 50, 'C': 100, 'D': 500, 'M': 1000
+}
+
+def roman_to_decimal_stack(stack):
+    if stack.size() == 0:
+        return 0
+
+    current = roman_values[stack.pop()]
+    if stack.size() == 0:
+        return current
+
+    next_value = roman_values[stack.on_top()]
+    if current > next_value:
+        return current - roman_to_decimal_stack(stack)
+    else:
+        return current + roman_to_decimal_stack(stack)
+
+def convertir_romano_con_stack(roman_str):
+    stack = Stack()
+    for ch in reversed(roman_str):
+        stack.push(ch)
+    return roman_to_decimal_stack(stack)
+
+# Prueba
+if __name__ == "__main__":
+    romano = 'XIV'
+    resultado = convertir_romano_con_stack(romano)
+    print(f'{romano} en decimal es {resultado}')
