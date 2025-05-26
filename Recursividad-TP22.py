e2401989086aa4b62248628fe44793aac9f12932
Marco Weiss <marcoweiss22@gmail.com>
2025-05-26 20:43:19 -0300

    Update Recursividad-TP22.py

--- Recursividad-TP22.py
+++ Recursividad-TP22.py
@@ -1,25 +1,22 @@
-def usar_la_fuerza(mochila, objetos_retirados=0):
-    # Caso base: mochila vacía
-    if len(mochila) == 0:
-        return False, objetos_retirados
 
-    # Sacamos el primer objeto
-    objeto = mochila.pop(0)
-    objetos_retirados += 1
+from queue_ import Queue
 
-    # ¿Encontramos el sable de luz?
-    if objeto == "sable de luz":
-        return True, objetos_retirados
+def usar_la_fuerza_queue(queue, contador=0):
+    if queue.size() == 0:
+        return False, contador
+    item = queue.attention()
+    contador += 1
+    if item == 'sable de luz':
+        return True, contador
+    return usar_la_fuerza_queue(queue, contador)
+
+if __name__ == "__main__":
+    mochila = Queue()
+    for item in ['comunicador', 'comida', 'sable de luz', 'cuerda', 'manta']:
+        mochila.arrive(item)
+
+    encontrado, pasos = usar_la_fuerza_queue(mochila)
+    if encontrado:
+        print(f'Sable encontrado tras sacar {pasos} objetos.')
     else:
-        # Llamada recursiva con el resto de la mochila
-        return usar_la_fuerza(mochila, objetos_retirados)
-
-# Ejemplo de uso:
-mochila = ["comida", "botiquín", "comunicador", "sable de luz", "manta"]
-
-encontrado, cantidad = usar_la_fuerza(mochila.copy())
-
-if encontrado:
-    print(f"¡Sable de luz encontrado después de sacar {cantidad} objetos!")
-else:
-    print("No había sable de luz en la mochila.")
+        print('No se encontró el sable.')
